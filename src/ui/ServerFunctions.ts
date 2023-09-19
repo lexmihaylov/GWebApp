d6dc80504649a8ebec0314c55f4701b07313da51
Plamen Nedev <plamen.nedev@scalefocus.com>
2023-09-19 14:17:54 +0300

    initial commit

---
+++ src/ui/ServerFunctions.ts
@@ -0,0 +1,6 @@
+/**
+ * Define functions that can be executed on the backend of the web app
+ */
+export interface ServerFunctions {
+    
+}
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,9 +1,6 @@
 import './styles/style.scss';
+import { ServerFunctions } from './ServerFunctions';
 import { ServerScriptAsync } from "gwebapp/browser/utils";
-
-interface ServerFunctions {
-    saveRecord: (firstName: string, lastName: string, UCN: string) => any;
-}
 
 (function() {
     // TODO: UI code goes here
