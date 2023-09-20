efd01a7a75ba1bc536f2a7feac4f4044da64da74
Plamen Nedev <plamen.nedev@scalefocus.com>
2023-09-20 10:37:27 +0300

    Initial commit

--- gwebapp/browser/utils.ts
+++ gwebapp/browser/utils.ts
@@ -16,8 +16,8 @@ export function ServerScript<T>(): GServerScripts<GServerScripts<T> & T> & T {
     return google.script.run as GServerScripts<GServerScripts<T> & T> & T;
 }
 
-export async function ServerScriptAsync<T>(serverFunction: keyof T, ...args) {
-    return new Promise((resolve, reject) => {
+export async function ServerScriptAsync<T, U>(serverFunction: keyof T, ...args) {
+    return new Promise<U>((resolve, reject) => {
         google.script.run.withFailureHandler((data) => {
             reject(data);
         }).withSuccessHandler((data) => {
